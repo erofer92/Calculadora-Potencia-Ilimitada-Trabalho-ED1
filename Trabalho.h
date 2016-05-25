151e338d0356b7e2e59d84612b8f2268ebdb50c6
Eron <erofer92@gmail.com>
2016-05-25 17:53:25 -0300

    Delete Trabalho.h
    Este arquivo foi substituido por Exponenciacao.h

--- Trabalho.h
+++
@@ -1,9 +0,0 @@
-
-
-void menu(Lista *numero, Lista *potencia);
-Lista * numero_para_lista(int n);
-Lista * somar(Lista *operando1, Lista *operando2);
-Lista * multiplicar(Lista * lista, int numero);
-Lista * multiplicar_listas(Lista *operando1, Lista *operando2);
-
-Lista * potenciacao(Lista *numero, int pot);
